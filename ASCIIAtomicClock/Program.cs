e83e1c00cf053928c0496b958730004d2777a900
Douglas Feaster <Douglas.Feaster@johnsonu.edu>
2020-05-03 11:42:11 -0400

    Add project files.

---
+++ ASCIIAtomicClock/Program.cs
@@ -0,0 +1,30 @@
+﻿using Colorful;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+
+namespace ASCIIAtomicClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FigletFont font = FigletFont.Load("standard.flf");
+            Figlet figlet = new Figlet(font);
+
+            while (true)
+            {
+                Console.WriteLine(figlet.ToAscii(DateTime.Now.ToString("hh:mm:ss")), ColorTranslator.FromHtml("#00cc00"));
+                Thread.Sleep(65);
+                Console.Clear();
+            }
+
+        }
+    }
+}
